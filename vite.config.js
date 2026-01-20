0e77c01b79daec4577cb0d4dfd3221e2f4dc7a11
Alberto Carlos <albertocarlosailisoaie@gmail.com>
2026-01-20 09:22:56 +0100

    WIP

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
+  build: {
+    rollupOptions: {
+      output: {
+        // Asegurar que sitemap.xml se copie correctamente
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'sitemap.xml' || assetInfo.name === 'robots.txt') {
+            return '[name][extname]'
+          }
+          return 'assets/[name]-[hash][extname]'
+        }
+      }
+    }
+  }
 })
